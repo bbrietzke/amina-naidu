1c177f5a277a1da691615683b8a159df4ecce71d
Brian Brietzke <bbrietzke+github@gmail.com>
2022-03-08 10:31:14 -0600

    Working on unit test.

--- Makefile
+++ Makefile
@@ -4,5 +4,5 @@ run:
 	python src/main.py
 
 test:
-	rm -rf /tmp/test_add_game_for_week.sqlite3
+	rm -f /tmp/test_add_game_for_week.sqlite3 /tmp/test_empty_games.sqlite3
 	python src/test_league_manager.py
--- src/test_league_manager.py
+++ src/test_league_manager.py
@@ -6,7 +6,8 @@ from lib.database_service import InMemoryDatabase, DatabaseService
 
 class TestLeagueManager(unittest.TestCase):
     def test_empty_games(self):
-        with InMemoryDatabase() as db:
+        with DatabaseService('/tmp/test_empty_games.sqlite3') as db:
+            db.executescript(DatabaseService.database_ddl())
             with LeagueManager(db) as lm:
                 results = lm.show_current_game()
                 self.assertIsNone(results)
@@ -17,8 +18,8 @@ class TestLeagueManager(unittest.TestCase):
             db.executescript(DatabaseService.database_ddl())
             with LeagueManager(db) as lm:
                 self.assertGreater(lm.save_game(0, start_week = this_week - 1, url = "https://example.org/game/1", title = "testing 1"), 0)
-                self.assertGreater(lm.save_game(0, start_week = this_week, url = "https://example.org/game/2", title = "testing 2"), 0)
-                self.assertGreater(lm.save_game(0, start_week = this_week + 1, url = "https://example.org/game/3", title = "testing 3"), 0)
+                self.assertGreater(lm.save_game(0, start_week = this_week, url = "https://example.org/game/2", title = "testing 2"), 1)
+                self.assertGreater(lm.save_game(0, start_week = this_week + 1, url = "https://example.org/game/3", title = "testing 3"), 2)
 
                 game = lm.show_current_game()
                 self.assertIsNotNone(game, "we don't have a game to display")
